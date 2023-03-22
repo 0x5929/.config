fc6660d8bf15690ac1f2f850a35e18dd10d50957
kevin <work.kevin.ren@gmail.com>
2023-03-21 19:33:24 -0700

    was able to compile, but cannot get search prompt to come up, not sure why, need to find out next time

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -26,6 +26,8 @@ import qualified Data.Map        as M
 import Data.Char (toUpper) 
 import Data.Maybe (fromJust)
 import Data.Maybe (isJust)
+import Data.Char (isSpace)
+
 
 -- not sure if the modules below are needed, uncomment as see fit
 -- import Data.Tree
@@ -73,6 +75,16 @@ import XMonad.Layout.WindowNavigation                                           
 import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))     -- allows toggling between layouts
 import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
 
+-- Prompt
+import XMonad.Prompt
+import XMonad.Prompt.Input
+import XMonad.Prompt.FuzzyMatch
+import XMonad.Prompt.Man
+import XMonad.Prompt.Pass
+import XMonad.Prompt.Shell (shellPrompt)
+import XMonad.Prompt.Ssh
+import XMonad.Prompt.XMonad
+import Control.Arrow (first)
 
 
 -- Utilities
@@ -150,6 +162,9 @@ myFocusColor  = color15     -- This variable is imported from Colors.THEME
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = False
 
+
+altMask :: KeyMask
+altMask = mod1Mask         -- Setting this for use in xprompts
 
 
 -- counting windows 
@@ -313,6 +328,142 @@ gsUtilities = [
    ("Calculator", "gnome-calculator")
   , ("Vim", (myTerminal ++ " -e vim"))
   ]
+
+
+------------------------------------------------------------------------
+-- XPROMPT SETTINGS
+------------------------------------------------------------------------
+dtXPConfig :: XPConfig
+dtXPConfig = def
+      { font                = myFont
+      , bgColor             = "#292d3e"
+      , fgColor             = "#d0d0d0"
+      , bgHLight            = "#c792ea"
+      , fgHLight            = "#000000"
+      , borderColor         = "#535974"
+      , promptBorderWidth   = 0
+      , promptKeymap        = dtXPKeymap
+      , position            = Top
+--    , position            = CenteredAt { xpCenterY = 0.3, xpWidth = 0.3 }
+      , height              = 20
+      , historySize         = 256
+      , historyFilter       = id
+      , defaultText         = []
+      , autoComplete        = Just 100000  -- set Just 100000 for .1 sec
+      , showCompletionOnTab = False
+      -- , searchPredicate     = isPrefixOf
+      , searchPredicate     = fuzzyMatch
+      , alwaysHighlight     = True
+      , maxComplRows        = Nothing      -- set to Just 5 for 5 rows
+      }
+
+-- The same config above minus the autocomplete feature which is annoying
+-- on certain Xprompts, like the search engine prompts.
+dtXPConfig' :: XPConfig
+dtXPConfig' = dtXPConfig
+      { autoComplete        = Nothing
+      }
+
+-- A list of all of the standard Xmonad prompts and a key press assigned to them.
+-- These are used in conjunction with keybinding I set later in the config.
+promptList :: [(String, XPConfig -> X ())]
+promptList = [ ("m", manPrompt)          -- manpages prompt
+             , ("p", passPrompt)         -- get passwords (requires 'pass')
+             , ("g", passGeneratePrompt) -- generate passwords (requires 'pass')
+             , ("r", passRemovePrompt)   -- remove passwords (requires 'pass')
+             , ("s", sshPrompt)          -- ssh prompt
+             , ("x", xmonadPrompt)       -- xmonad prompt
+             ]
+
+-- Same as the above list except this is for my custom prompts.
+promptList' :: [(String, XPConfig -> String -> X (), String)]
+promptList' = [ ("c", calcPrompt, "qalc")         -- requires qalculate-gtk
+              ]
+
+------------------------------------------------------------------------
+-- CUSTOM PROMPTS
+------------------------------------------------------------------------
+-- calcPrompt requires a cli calculator called qalcualte-gtk.
+-- You could use this as a template for other custom prompts that
+-- use command line programs that return a single line of output.
+calcPrompt :: XPConfig -> String -> X ()
+calcPrompt c ans =
+    inputPrompt c (trim ans) ?+ \input ->
+        liftIO(runProcessWithInput "qalc" [input] "") >>= calcPrompt c
+    where
+        trim  = f . f
+            where f = reverse . dropWhile isSpace
+
+------------------------------------------------------------------------
+-- XPROMPT KEYMAP (emacs-like key bindings for xprompts)
+------------------------------------------------------------------------
+dtXPKeymap :: M.Map (KeyMask,KeySym) (XP ())
+dtXPKeymap = M.fromList $
+     map (first $ (,) controlMask)   -- control + <key>
+     [ (xK_z, killBefore)            -- kill line backwards
+     , (xK_k, killAfter)             -- kill line forwards
+     , (xK_a, startOfLine)           -- move to the beginning of the line
+     , (xK_e, endOfLine)             -- move to the end of the line
+     , (xK_m, deleteString Next)     -- delete a character foward
+     , (xK_b, moveCursor Prev)       -- move cursor forward
+     , (xK_f, moveCursor Next)       -- move cursor backward
+     , (xK_BackSpace, killWord Prev) -- kill the previous word
+     , (xK_y, pasteString)           -- paste a string
+     , (xK_g, quit)                  -- quit out of prompt
+     , (xK_bracketleft, quit)
+     ]
+     ++
+     map (first $ (,) altMask)       -- meta key + <key>
+     [ (xK_BackSpace, killWord Prev) -- kill the prev word
+     , (xK_f, moveWord Next)         -- move a word forward
+     , (xK_b, moveWord Prev)         -- move a word backward
+     , (xK_d, killWord Next)         -- kill the next word
+     , (xK_n, moveHistory W.focusUp')   -- move up thru history
+     , (xK_p, moveHistory W.focusDown') -- move down thru history
+     ]
+     ++
+     map (first $ (,) 0) -- <key>
+     [ (xK_Return, setSuccess True >> setDone True)
+     , (xK_KP_Enter, setSuccess True >> setDone True)
+     , (xK_BackSpace, deleteString Prev)
+     , (xK_Delete, deleteString Next)
+     , (xK_Left, moveCursor Prev)
+     , (xK_Right, moveCursor Next)
+     , (xK_Home, startOfLine)
+     , (xK_End, endOfLine)
+     , (xK_Down, moveHistory W.focusUp')
+     , (xK_Up, moveHistory W.focusDown')
+     , (xK_Escape, quit)
+     ]
+
+
+
+-- Search feature settings
+-- Xmonad has several search engines available to use located in
+-- XMonad.Actions.Search. Additionally, you can add other search engines
+-- such as those listed below.
+-- archwiki, ebay, news, reddit, urban :: S.SearchEngine
+
+-- archwiki = S.searchEngine "archwiki" "https://wiki.archlinux.org/index.php?search="
+-- ebay     = S.searchEngine "ebay" "https://www.ebay.com/sch/i.html?_nkw="
+-- news     = S.searchEngine "news" "https://news.google.com/search?q="
+-- reddit   = S.searchEngine "reddit" "https://www.reddit.com/search/?q="
+-- urban    = S.searchEngine "urban" "https://www.urbandictionary.com/define.php?term="
+
+-- This is the list of search engines that I want to use. Some are from
+-- XMonad.Actions.Search, and some are the ones that I added above.
+searchList :: [(String, S.SearchEngine)]
+searchList = [ ("d", S.duckduckgo)
+             , ("g", S.google)
+             , ("h", S.hoogle)
+             , ("i", S.images)
+             , ("t", S.thesaurus)
+             , ("v", S.vocabulary)
+             , ("w", S.wikipedia)
+             , ("y", S.youtube)
+             , ("z", S.amazon)
+             ]
+
 
 
 
@@ -490,35 +641,6 @@ myLayoutHook = avoidStruts
 myLogHook :: X ()
 myLogHook = fadeInactiveLogHook fadeAmount
    where fadeAmount = 0.9
-
-
-
--- Search feature settings
--- Xmonad has several search engines available to use located in
--- XMonad.Actions.Search. Additionally, you can add other search engines
--- such as those listed below.
--- archwiki, ebay, news, reddit, urban :: S.SearchEngine
-
--- archwiki = S.searchEngine "archwiki" "https://wiki.archlinux.org/index.php?search="
--- ebay     = S.searchEngine "ebay" "https://www.ebay.com/sch/i.html?_nkw="
--- news     = S.searchEngine "news" "https://news.google.com/search?q="
--- reddit   = S.searchEngine "reddit" "https://www.reddit.com/search/?q="
--- urban    = S.searchEngine "urban" "https://www.urbandictionary.com/define.php?term="
-
--- This is the list of search engines that I want to use. Some are from
--- XMonad.Actions.Search, and some are the ones that I added above.
-searchList :: [(String, S.SearchEngine)]
-searchList = [ ("d", S.duckduckgo)
-             , ("g", S.google)
-             , ("h", S.hoogle)
-             , ("i", S.images)
-             , ("t", S.thesaurus)
-             , ("v", S.vocabulary)
-             , ("w", S.wikipedia)
-             , ("y", S.youtube)
-             , ("z", S.amazon)
-             ]
-
 
 
 
@@ -757,6 +879,11 @@ myKeys c =
   , ("<Print>", addName "Take screenshot (dmscripts)" $ spawn "dm-maim")
   ]
 
+  -- ^++^ [("M-s-" ++ k, addName "promptSearch"  $ S.promptSearch dtXPConfig' f ) | (k,f) <- searchList ]
+  ^++^ subKeys "prompSearch"
+  [("M-s g", addName "promptSearch google"  $ S.promptSearch dtXPConfig' S.google) 
+  ,("M-S-s g", addName "selectSearch google" $ S.selectSearch S.google)
+  ]
   -- Appending search engine prompts to keybindings list.
   -- Look at "search engines" section of this config for values for "k".
  --   need to figure how to get this running
